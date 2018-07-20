4dcb598fdfd4af6d9c0ff73b3dad217070aafa9f
James Washington <jbwashington@gmail.com>
2018-07-19 21:24:49 -0400

    integrated ytdl and flask app

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,70 @@
 #!flask/bin/python
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_restful import Resource, Api
+import youtube_dl
+
+class MyLogger(object):
+    """logs output from youtube """
+    def debug(self, msg):
+        pass
+    def warning(self, msg):
+        pass
+    def error(self, msg):
+        print(msg)
+
+def my_hook(d):
+    if d['status'] == 'finished':
+        print('Done downloading, now converting ...')
+
+ydl_opts = {
+        'logger'                     : MyLogger(), # logs output by youtubedl
+        'progress_hooks'             : [my_hook], # tells status in console
+        # 'default-search'           : 'ytsearch', # search youtube if error in with url
+        'youtube-skip-dash-manifest' : True, # skip recommended videos
+        'restrict-filenames'         : True, # no ASCII ampersands or spaces
+        'download'                   : False, # no download for test purposes
+        'format'                     : 'bestaudio/best[ext!=webm]', # get the best quality
+        'extractaudio'               : True,  # only keep the audio
+        'audioformat'                : 'mp3',  # convert to mp3
+        'outtmpl'                    : '%(id)s',    # name the file the ID of the video
+        'noplaylist'                 : True,    # only download single song, not playlist
+        }
+
+def get_track(title):
+
+    title = "ytsearch:" + title
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+
+        track = ydl.download([title])
+
+        meta = ydl.extract_info(title)
+
+        return track
+
+
+def get_info(title):
+
+    title = "ytsearch:" + title
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+
+        info = ydl.extract_info(title)
+
+        return info
+
+
+
+
+
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
@@ -36,5 +95,14 @@ def index():
     output = user_schema.dump(one_user).data # serialize python object into json
     return jsonify({'user' : output})
 
+@app.route('/track/<track_name>')
+def get_track_info(track_name):
+    # one_user = User.query.first()
+    # user_schema = UserSchema()
+    # output = user_schema.dump(one_user).data # serialize python object into json
+    track_info = get_info(track_name)
+    return jsonify({ 'track_info' : track_info })
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
+
