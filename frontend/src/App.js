5b8438816810ac91bc2e205fa7e02e152b154837
James Washington <jbwashington@gmail.com>
2018-07-25 03:26:27 -0400

    added audio player controls

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import Config               from './config';
 import SearchBar            from './components/SearchBar';
 import VideoDetail          from './components/VideoDetail';
 import VideoList            from './components/VideoList';
-import SongQueue            from './components/SongQueue';
+import Player               from './components/Player';
 import Footer               from './components/Footer';
 
 import {
@@ -20,7 +20,6 @@ import {
   Container,
   Col,
   Row,
-  Button
 } from 'reactstrap';
 
 const REACT_APP_API_KEY = Config.YT_API_KEY;
@@ -32,9 +31,39 @@ class App extends Component {
     this.videoSearch('type beat'); // default search term
     this.state = {
       videos: [], // holds 5 videos fetched from API
+      playlist: this.props.data,
       selectedVideo: null,
+      addSongTitle: null,
+      addSongURL: null,
+      playing: false,
       isOpen: false
     };
+  }
+
+  addSongTitleChange(e){
+    this.setState({
+      addSongTitle: e.target.value
+    });
+  }
+
+  addSongURLChange(e){
+    this.setState({
+      addSongURL: e.target.value
+    });
+  }
+
+  addSong(){
+    this.state.playlist.push({
+      id: this.state.playlist[this.state.playlist.length - 1].id + 1,
+      title: this.state.addSongTitle,
+      url: this.state.addSongURL,
+    });
+
+    this.setState({
+      videos: this.state.videos,
+      addSongTitle: '',
+      addSongURL: ''
+    });
   }
 
   toggle() {
@@ -56,63 +85,56 @@ class App extends Component {
     );
   }
 
-  render() {
-    // for consistent ui such that it re-renders after 300ms on search
-    const videoSearch = debounce(term => {
-      this.videoSearch(term);
-    }, 600);
+render() {
+  // for consistent ui such that it re-renders after 300ms on search
+  const videoSearch = debounce(term => {
+    this.videoSearch(term);
+  }, 600);
 
-    return (
 
-      <div>
-        <Navbar color="inverse" light expand="md">
-          <NavbarBrand href="/">FINESSE.FM</NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink href="#">Register</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink href="#">Sign In</NavLink>
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </Navbar>
+  return (
 
-        <Container>
-          <Row>
+    <div>
+      <Navbar color="inverse" light expand="md">
+        <NavbarBrand href="/">FINESSE.FM</NavbarBrand>
+        <NavbarToggler onClick={this.toggle} />
+        <Collapse isOpen={this.state.isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="#">Register</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#">Sign In</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
 
-            <Col md="4">
-              <SearchBar onSearchTermChange={videoSearch} />
-              <VideoDetail video={this.state.selectedVideo} />
-              <VideoList
-                videos={ this.state.videos }
-                onVideoSelect = { selectedVideo => {
-                  this.setState({ selectedVideo });
-                }
-                }
-              />
-            </Col>
+      <Container>
+        <Row>
 
-            <Col md="8">
-              <Button
-                tag="a"
-                className="btn btn-primary"
-                size="small"
-                href="#"
-                target="_blank"
-              > Clear Playlist
-              </Button>
-              <SongQueue playlist = {this.state.selectedPlaylist} />
-            </Col>
-          </Row>
-        </Container>
+          <Col md="4">
+            <SearchBar onSearchTermChange={videoSearch} />
+            <VideoDetail video={this.state.selectedVideo} />
+            <VideoList
+              videos={ this.state.videos }
+              onVideoSelect = { selectedVideo => {
+                this.setState({ selectedVideo });
+              }
+              }
+            />
+          </Col>
 
-        <Footer/>
-      </div>
-    );
-  }
+          <Col md="8">
+            <Player/>
+          </Col>
+        </Row>
+      </Container>
+
+      <Footer/>
+    </div>
+  );
+}
 }
 
 export default App;
---
+++ frontend/src/components/Playlist.js
@@ -0,0 +1,10 @@
+// import React from 'react';
+import './Playlist.css';
+
+const playlistItems = this.videos.map(video => {
+  return (
+    {playlistItems}
+  );
+})
+
+export default playlistItems;
